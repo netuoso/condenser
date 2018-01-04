59f40b16222827279dc3bd938d1ca27a06e48ba1
Andrew Chaney (netuoso) <andrewc@pobox.com>
2018-01-03 20:35:02 -0600

    remove flag voting box from comment

--- src/app/components/cards/Comment.jsx
+++ src/app/components/cards/Comment.jsx
@@ -409,7 +409,6 @@ class CommentImpl extends React.Component {
                     </div>
                     <div className="Comment__header">
                         <div className="Comment__header_collapse">
-                            <Voting post={post} flag />
                             <a
                                 title={tt('g.collapse_or_expand')}
                                 onClick={this.toggleCollapsed}
